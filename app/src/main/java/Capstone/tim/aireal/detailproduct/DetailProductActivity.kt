bfd4af1cbc5e06fdc7a483c65a24ccb5c85d897b
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2024-06-03 20:54:37 +0800

    feature: create layout detail product page

---
+++ app/src/main/java/Capstone/tim/aireal/detailproduct/DetailProductActivity.kt
@@ -0,0 +1,12 @@
+package Capstone.tim.aireal.detailproduct
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import Capstone.tim.aireal.R
+
+class DetailProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_product)
+    }
+}
