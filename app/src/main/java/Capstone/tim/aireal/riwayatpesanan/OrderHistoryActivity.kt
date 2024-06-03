a73fa1f288208b6cd3b08c970c1949f3bb99ee57
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2024-06-03 13:49:28 +0800

    feature: create an order history page

---
+++ app/src/main/java/Capstone/tim/aireal/riwayatpesanan/OrderHistoryActivity.kt
@@ -0,0 +1,12 @@
+package Capstone.tim.aireal.riwayatpesanan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import Capstone.tim.aireal.R
+
+class OrderHistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order_history)
+    }
+}
