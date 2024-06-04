e98356d14f9831f7587a73df1c8b29b6deeda5c8
RifqiMuafa20 <muhammadrifqi20001@gmail.com>
2024-06-04 17:44:20 +0800

    feature: create explore page with search view inside and also refactor code in another page

---
+++ app/src/main/java/Capstone/tim/aireal/explore/ExploreActivity.kt
@@ -0,0 +1,12 @@
+package Capstone.tim.aireal.explore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import Capstone.tim.aireal.R
+
+class ExploreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explore)
+    }
+}
